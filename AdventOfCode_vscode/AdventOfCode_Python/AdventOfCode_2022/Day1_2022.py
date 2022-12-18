531d88d0849b3c17c767a18761b9f45dc56948e8
KC Lee <kckckckckckc@gmail.com>
2022-12-17 18:18:15 -0600

    Setup vscode and 2022.

---
+++ AdventOfCode_vscode/AdventOfCode_Python/AdventOfCode_2022/Day1_2022.py
@@ -0,0 +1,7 @@
+class Day1_2022:
+    def adventOfCode(self, s:str) -> str:
+        return s
+
+
+toSolution = Day1_2022()
+print(toSolution.adventOfCode("Hello"))
---
+++ AdventOfCode_vscode/AdventOfCode_Python/test.py
@@ -0,0 +1,7 @@
+class Solution:
+    def adventOfCode(self, s:str) -> str:
+        return s
+
+
+toSolution = Solution()
+print(toSolution.adventOfCode("Hello"))
